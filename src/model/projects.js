91e939dc94f0f49acf06f4fa95c1099e0f981778
Federico Knüssel <fknussel@gmail.com>
2016-09-25 22:59:34 +1000

    Update Bacon.js project outline

--- src/model/projects.js
+++ src/model/projects.js
@@ -1,51 +1,21 @@
-// baconjs: https://baconjs.github.io/logo.png
-// flux: https://worldvectorlogo.com/logos/flux.svg
-// redux: https://raw.githubusercontent.com/reactjs/redux/master/logo/logo.png
-// react: http://formatjs.io/img/react.svg
-// rx: https://avatars3.githubusercontent.com/u/6407041?v=3&s=400
-
 const projects = [
   {
     id: 'introduction-to-baconjs',
     image: 'https://baconjs.github.io/logo.png',
-    title: 'Functional Reactive Programming with Bacon.js',
-    content: 'This series of blog posts aims at providing an introduction to Functional Reactive Programming (FRP) in JavaScript using Bacon.js. It goes through the very basics of Observables, from creating them to manipulating and combining them.',
+    title: 'Introduction to Functional Reactive Programming with Bacon.js',
+    content: 'This is a series of blog posts on Functional Reactive Programming (FRP) in JavaScript using Bacon.js. It goes through the very basics of Observables, from creating them to manipulating and combining them.',
     links: [
       {
         title: 'Wrapping things in Bacon',
         url: '//medium.com/@fknussel/wrapping-things-in-bacon-27658075ed80',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        description: 'Learn to create event streams from multiple sources: DOM events, promises, timers and a bunch of others.'
       },
       {
         title: 'Event Streams vs Properties in Bacon.js',
         url: '//medium.com/@fknussel/event-streams-vs-properties-e55b53be8f42',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        description: 'Brief overview of what properties are and how they are different from event streams. We also go through how to create them and how to convert event streams into properties and viceversa.'
       }
     ]
-  },
-  {
-    id: 'something-flux',
-    image: 'https://worldvectorlogo.com/logos/flux.svg',
-    title: 'Something Flux',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  },
-  {
-    id: 'something-redux',
-    image: 'https://raw.githubusercontent.com/reactjs/redux/master/logo/logo.png',
-    title: 'Something Redux',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  },
-  {
-    id: 'something-react',
-    image: 'http://formatjs.io/img/react.svg',
-    title: 'Something React',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  },
-  {
-    id: 'something-rx',
-    image: 'https://avatars3.githubusercontent.com/u/6407041?v=3&s=400',
-    title: 'Something Rx',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   }
 ];
 
