3853549c304495739cbf0848f0918f705de83daa
Federico Knüssel <fknussel@gmail.com>
2016-12-09 00:23:28 -0300

    Update project descriptions

--- build/bundle.js
+++ build/bundle.js
@@ -23336,27 +23336,24 @@
 	  id: 'javascript-fundamentals',
 	  image: _javascript2.default,
 	  title: 'JavaScript Fundamentals',
-	  links: [{
-	    title: 'ES6 Overview',
-	    url: '//medium.com/@fknussel/wrapping-things-in-bacon-27658075ed80',
-	    description: 'Learn to create event streams from multiple sources: DOM events, promises, timers and a bunch of others.'
-	  }]
+	  content: 'Here\'s a bunch of articles on JavaScript, just a collection of concepts/thoughts/examples I\'d like to keep track of and think are worth sharing.',
+	  links: []
 	}, {
 	  id: 'jsort',
 	  image: _jsort2.default,
 	  title: 'jsort',
-	  content: 'Simple array sorting utility supporting multiple data types.',
+	  content: 'jsort is a simple array sorting utility library for JavaScript. You can use jsort in your ES6/CommonJS/AMD projects or directly in your browser. Just pass in an array along with all the options you need to set, and jsort will return a sorted array for you. jsort is less than 1 Kb, has a 100% test coverage, and a small and intuitive API which will make including it in your project really easy.',
 	  links: [{
-	    title: 'Check it out',
+	    title: 'Check it out on GitHub',
 	    url: '//github.com/fknussel/jsort'
 	  }]
 	}, {
 	  id: 'react-redux-boilerplate',
 	  image: _react2.default,
 	  title: 'React & Redux Boilerplate',
-	  content: 'Simple array sorting utility supporting multiple data types.',
+	  content: 'This is a simple boilerplate based on my usual configuration for React projects. It features React.js as the UI library, Redux to handle the application state, React Router as the routing library, Babel to transpile ES6/JSX/Flow code to plain old JavaScript, Webpack to bundle all code together, Immutable.js to model state on reducers, CSS modules and Post CSS for styling, eslint and stylelint for linting the codebase and Jest and Enzyme for unit and functional testing.',
 	  links: [{
-	    title: 'Check it out',
+	    title: 'Check it out on GitHub',
 	    url: '//github.com/fknussel/react-redux-boilerplate'
 	  }]
 	}];
