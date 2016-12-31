e5427e1d35fbc6a2224f7eb3b7d0c042a7a4fb7b
Federico Knüssel <fknussel@gmail.com>
2016-12-31 15:26:25 -0300

    More clean up work

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import projects from './model/projects';
 
 ReactDOM.render(
-  <App projects={projects} />,
+  <App />,
   document.getElementById('app')
 );
