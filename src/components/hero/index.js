d202d8ca2e1f78bd06fdb233d4cc8a0837cef28d
Federico Knüssel <fknussel@gmail.com>
2017-01-01 17:37:35 -0300

    Tweak Hero component, drop headline

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -6,9 +6,8 @@ const email = 'fknussel@gmail.com';
 const Hero = () => (
   <header className={styles.hero}>
     <h1 className={styles.title}>Federico Knüssel</h1>
-    <h2 className={styles.subtitle}>Software Engineer</h2>
     <p className={styles.content}>
-      I'm a Front End Developer with a keen interest in JavaScript and Functional Reactive Programming, and I help development teams build maintainable and scalable web and mobile apps. Feel free to hit me up on <a href={`mailto:${email}`}>{email}</a>.
+      I'm a Front End Developer with a keen interest in JavaScript and Functional Reactive Programming, and I help development teams build maintainable and scalable web apps. Feel free to hit me up on <a href={`mailto:${email}`}>{email}</a>.
     </p>
   </header>
 );
