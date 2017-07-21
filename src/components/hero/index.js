3278c91930eac1464083fb707e0e06777681f7cc
Federico Knüssel <fknussel@gmail.com>
2017-07-21 21:05:23 +0200

    Update Hero text

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -7,7 +7,7 @@ const Hero = () => (
   <header className={styles.hero}>
     <h1 className={styles.title}>Federico Knüssel</h1>
     <p className={styles.content}>
-      I'm a software developer with a keen interest in JavaScript and functional programming, and I help development teams ship maintainable and reliable code. Feel free to hit me up on <a href={`mailto:${email}`}>{email}</a>.
+      I'm a software developer with a keen interest in JavaScript and functional programming, and I help development teams ship clean and maintainable code. Feel free to hit me up on <a href={`mailto:${email}`}>{email}</a>.
     </p>
   </header>
 );
